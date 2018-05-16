f3e2b2986a32fd0873021a1cfdf0f62836f68778
k-solovev <k.solovev.work@gmail.com>
2018-05-17 01:14:28 +0300

    закончил эффекты наведения и исправления

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -3,9 +3,9 @@ var mobileMenu = document.querySelector(".main-nav__list");
 
 menuBtn.addEventListener("click", function (evt) {
 
-  if (mobileMenu.classList.contains('main-nav--close')) {
-    mobileMenu.classList.remove('main-nav--close');
-    mobileMenu.classList.add('main-nav--open');
+  if (mobileMenu.classList.contains('main-nav__list--close')) {
+    mobileMenu.classList.remove('main-nav__list--close');
+    mobileMenu.classList.add('main-nav__list--open');
 
     if (menuBtn.classList.contains('btn--close')) {
       menuBtn.classList.remove('btn--close');
@@ -13,8 +13,8 @@ menuBtn.addEventListener("click", function (evt) {
     }}
 
     else {
-    mobileMenu.classList.add('main-nav--close');
-    mobileMenu.classList.remove('main-nav--open');
+    mobileMenu.classList.add('main-nav__list--close');
+    mobileMenu.classList.remove('main-nav__list--open');
 
     if (menuBtn.classList.contains('btn--open')) {
       menuBtn.classList.remove('btn--open');
